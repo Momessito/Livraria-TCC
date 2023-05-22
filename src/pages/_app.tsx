1527e35204e6d4bd3fd366d5187e2b992a5cbedf
felipe Momesso <pinhoes1000@gmail.com>
2023-05-22 12:46:46 -0300

    Mudança layout - Felipe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,8 @@
 import '@/styles/globals.css'
+import "@/styles/section1.css"
+import "animate.css/animate.min.css";
+
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
